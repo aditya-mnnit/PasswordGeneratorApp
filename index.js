37d232ecf307e260debb8e0097cbe35381833feb
Aditya Singh Yadav <adityainprivate@gmail.com>
2023-07-19 03:33:44 +0530

    3am

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import fs from "fs";
 import path from "path";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
 mongoose.connect("mongodb://localhost:27017",{
     dbName:"backend",
 }).then(()=>console.log("Database Connected")).catch((e)=>console.log(e));
@@ -13,16 +14,20 @@ app.use(express.static(path.join(path.resolve(),"public")));//we make public sta
 app.use(cookieParser());
 app.set("view engine","ejs");  
 
-const msgSchema=new mongoose.Schema({
-    name:String,
+const userSchema=new mongoose.Schema({
+    password:String,
     email:String,
 });
-const msg=mongoose.model("Message",msgSchema);
-const isauth=(req,res,next)=>
+
+const usr=mongoose.model("User",userSchema);
+const isauth=async(req,res,next)=>
 {
     const {token}=req.cookies;
     if(token)
     {
+        const jwtdecoded=jwt.verify(token,"secret");
+        req.user=await usr.findById(jwtdecoded._id);
+         
         // res.render("logout");
         next();
     }
@@ -31,27 +36,41 @@ const isauth=(req,res,next)=>
     }
 }
 app.get("/",isauth,(req,res)=>{
+    console.log(req.user);
     console.log(req.cookies);
-    res.render("logout");
+    res.render("logout",{pass:req.user.password});
    
 
 });
 app.get("/add",async(req,res)=>{
-   await msg.create({
+   await usr.create({
         name:"aditya",
         email:"xydndgz@gmail.com",
     });
     res.send("Done");
 });
-app.post("/login",(req,res)=>
+app.get("/gen",(req,res)=>{
+    res.render("sample");
+})
+app.post("/login",async(req,res)=>
 {
-    res.cookie("token","iamin",{
+    // cont user=await(usr.findOne(req.body.email));
+
+   const usercreated= await usr.create({
+        password:req.body.password,
+        email:req.body.email,
+    });
+    const token=jwt.sign({_id:usercreated._id},"secret")
+    console.log(req.body);
+    res.cookie("token",token,{
         httpOnly: true,
         expires: new Date(Date.now()+6000),
     });
-    res.redirect("/");
+    res.redirect("/gen");
+    // res.redirect("/");
 });
 app.get("/logout",(req,res)=>{
+    
     res.cookie("token","null",{
         expires: new Date(Date.now()),
     });
